d9687097c9052a467f53f27b6845c89ef1325f81
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2023-03-01 13:28:16 +0100

    Update botadd.js

--- Events/Bot/botadd.js
+++ Events/Bot/botadd.js
@@ -1,13 +1,9 @@
 const Discord = require("discord.js")
 const Event = require("../../Structure/Event");
-const { REST } = require("@discordjs/rest")
-const { Routes } = require("discord-api-types/v9")
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const { token } = require("../../config")
 
 module.exports = new Event("guildCreate", async (bot, guild) => {
 
-    console.log(`New serveur : ${guild.name} je suis à  ${bot.guilds.size} serveur !`)
+    console.log(`New serveur : ${guild.name} je suis à ${bot.guilds.cache.size}  serveur !`)
     
     
       const channel = bot.channels.cache.get("")  

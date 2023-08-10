ea0de3a9f3f140b3692766e3a50aad7665318465
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2023-08-10 13:48:11 +0200

    Update ready.js

--- Events/Bot/ready.js
+++ Events/Bot/ready.js
@@ -22,13 +22,4 @@ module.exports = new Event("ready", async bot => {
 
 
     console.log(chalk.bgGreen(`${bot.user.username} : En ligne sur ${bot.guilds.cache.size} serveurs, surveille ${bot.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} membres, son ping : ${bot.ws.ping} ms !`))
-   
-    const channel = bot.channels.cache.get("1125390156468326480")  
-    const embed = new Discord.MessageEmbed()
-    .setColor('GREEN')
-    .setTitle("Bot démarré !")
-    .setDescription(`Bot sur \`\`${bot.guilds.cache.size}\`\` **serveurs,** surveille \`\`${bot.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}\`\` **membres !**`)
-    .setTimestamp()
-    .setFooter(`Le Ping : ${bot.ws.ping} ms...`)
-     channel.send({ embeds: [embed]})
 })

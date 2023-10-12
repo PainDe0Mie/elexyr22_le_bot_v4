cd6087083b6dc8701357e8a2e0e4dc1754a4ac6e
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2023-10-12 16:22:45 +0200

    Update pile-face.js

--- Commandes/fun/pile-face.js
+++ Commandes/fun/pile-face.js
@@ -1,6 +1,6 @@
-
 const Discord = require("discord.js")
 const Command = require("../../Structure/Command")
+const chalk = require("chalk")
 
 module.exports = new Command({
 
@@ -13,12 +13,13 @@ module.exports = new Command({
     cooldown: 5,
     async run(bot, message, args, db) {
 
-    let replies = ["Pile", "Face"];
+    let replies = ["Pile", "Face", "Pile", "Face", "Pile", "Face"]
 
     let res = Math.floor(Math.random() * replies.length);
     
 
-    message.reply(`La réponse est : || **${replies[res]} !** ||`)
+    message.reply(`Le **Résulat** est: || \`\`${replies[res]}\`\` ||`)
+    console.log(chalk.yellow(`[CMD] "${message.author.username}" à utilisé la commande e!pile-face sûr '${message.guild.name}'`))
    
 
 }});

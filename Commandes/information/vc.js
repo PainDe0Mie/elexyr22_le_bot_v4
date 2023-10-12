cc530372e2c04901ecd829ecace9569242231a99
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2023-10-12 16:27:46 +0200

    Update vc.js

--- Commandes/information/vc.js
+++ Commandes/information/vc.js
@@ -1,17 +1,40 @@
-const Discord = require("discord.js")
-const Command = require("../../Structure/Command")
+const Discord = require("discord.js");
+const Command = require("../../Structure/Command");
+const chalk = require("chalk");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = new Command({
+  name: "vocal",
+  description: "Nombre de personnes en vocal, cam, stream et mute",
+  utilisation: "",
+  alias: ["vocal", "vc"],
+  permission: "",
+  category: "2) Information",
+  cooldown: 5,
 
-    name: "vc",
-    description: "Nombre de personne en Vocal",
-    utilisation: "",
-    alias: ["vc"],
-    permission: "",
-    category: "2) Information",
-    cooldown: 5,
+  async run(bot, message, args, db) {
+    try {
+      const guild = await message.guild.fetch();
+      const members = guild.memberCount;
+      const onlineMembers = guild.presences.cache.filter((presence) => presence.status !== 'offline').size;
+      const voiceMembers = guild.channels.cache
+        .filter((channel) => channel.type === 'GUILD_VOICE' || channel.type === 'GUILD_STAGE_VOICE')
+        .reduce((acc, channel) => acc + channel.members.size, 0);
+      const boost = guild.premiumSubscriptionCount;
 
-    async run(bot, message, args, db) {
-        message.reply(`🎙️ | Membre en vocal: **${message.guild.members.cache.filter(m => m.voice.channel).size}** (**${message.guild.memberCount} membres**)`)
+      const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setThumbnail(guild.iconURL({ dynamic: true }))
+        .setTitle(`${guild.name}〃Statistiques`)
+        .setURL("https://discord.gg/elexyr22")
+        .setDescription(`Membres: **${members}** <:member1:1099984803937402990>\nEn ligne: **${onlineMembers}** <:online2:1099984887987048510>\nEn vocal: **${voiceMembers}** <:vocals3:1099984979666145350> \nBoost: **${boost}**<a:boost4:1099985023530172416>`)
+        .setFooter(`Demandé par : ${message.user ? message.user.username : message.author.username}`, message.user ? message.user.displayAvatarURL({dynamic: true}) : message.author.displayAvatarURL({dynamic: true}))
+      console.log(chalk.yellow(`[CMD] "${message.author.username}" a utilisé la commande e!vocal sur '${guild.name}'`));  
+    
+      await message.reply({embeds: [embed]});
+    } catch (error) {
+      console.error(error);
+      // Gérer les erreurs ici
     }
-})
+  }
+});

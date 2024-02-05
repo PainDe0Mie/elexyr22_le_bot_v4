50a6c5b7981f0b572b84320fad48efd1cd46f0c5
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-02-05 18:59:20 +0100

    Update ban-log.js

--- Events/Logs/ban-log.js
+++ Events/Logs/ban-log.js
@@ -5,16 +5,32 @@ const numberBanMap = new Map();
 
 module.exports = new Event("guildBanAdd", async (bot, ban, guild) => {
 
-    const audit = (await ban.guild.fetchAuditLogs().catch(() => {})).entries.first();
-    if(audit) return
-    
-    const reason = audit.reason
-             
-    if(audit.action === "MEMBER_BAN_ADD") {
-    if(audit.action === "MEMBER_BAN_ADD" == true){
+  const audit = (await ban.guild.fetchAuditLogs().catch(() => {})).entries.first();
+  if(audit) return;
+  
+
+	if(audit.action === "MEMBER_BAN_ADD") {
+  if(audit.action === "MEMBER_BAN_ADD" == true){
+      
+const reason = audit.reason
  
 
-console.log(chalk.red(`[NEW BAN :] (${ban.user.username}) à été ban par "${audit.executor.username}" pour <${reason}>  sûr le serveur '${ban.guild.name}'`))
-  //console.log(chalk.cyan(`[NEW MESSAGE :] ${member.username}  à envoyé le message "${message.content}" dans le channel : < ${message.channel.name} > sûr le serveur '${message.guild.name}' (${message.guildId})`))  
-    
-}}})
+const db = bot.db;
+
+console.log(chalk.green(`[NEW ban :] ${ban.user.username} à été ban par "${audit.executor.username}" sûr le serveur '${ban.guild.name}' pour "${reason}" `))
+
+let Embed = new Discord.MessageEmbed()
+.setColor("YELLOW")
+.setThumbnail(ban.user.displayAvatarURL({dynamic: true}))
+.setTitle("New Ban:")
+.setDescription(`**${ban.user} - ${ban.user.username}** *(${ban.user.id})* viens d'être ban par ${audit.executor} pour ${reason}`)
+ .setTimestamp()
+ 
+db.query(`SELECT * FROM serveur WHERE guildID = ${ban.guild.id}`, async (err, req) => {
+if(req.length < 1) return
+
+   let channel = ban.guild.channels.cache.get(`${req[0].logID}`)
+    if(!channel) return;
+    await channel.send({embeds: [Embed]})
+
+})}}})

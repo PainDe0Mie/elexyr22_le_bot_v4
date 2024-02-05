bf32f2d5ebc28cc825fea4af9c55fc365a9102f1
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-02-05 19:00:41 +0100

    Update voice-24-7.js

--- Events/Guild/voice-24-7.js
+++ Events/Guild/voice-24-7.js
@@ -3,16 +3,21 @@ const { joinVoiceChannel } = require('@discordjs/voice');
 const Event = require("../../Structure/Event");
 
 module.exports = new Event("ready", async (bot) => {
-  const voiceChannelId = "1119722076396326995";
+  const voiceChannelId = "1186064749113380946"
   if (!voiceChannelId) return;
 
   const voiceChannel = bot.channels.cache.get(voiceChannelId);
   if (!voiceChannel) {
-    console.log(`Le salon vocal avec l'ID ${voiceChannelId} est introuvable.`);
+    console.log(`Le salon auto-vocal existe pas`);
     return}
+    
+    console.log("Bot bien connecté au vocal Commu")
 
   const connection = joinVoiceChannel({
     channelId: voiceChannel.id,
     guildId: voiceChannel.guild.id,
     adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-  })})
+  })
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+})

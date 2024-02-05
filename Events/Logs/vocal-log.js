5ca4aac6ee68f89d18293930efcb5a6e69064330
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-02-05 19:00:14 +0100

    Update vocal-log.js

--- Events/Logs/vocal-log.js
+++ Events/Logs/vocal-log.js
@@ -1,41 +1,50 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const Event = require("../../Structure/Event");
 const chalk = require("chalk");
 
 module.exports = new Event("voiceStateUpdate", async (bot, oldState, newState) => {
     
-    if(newState.member.user.id === "1013135812545753119") return
+  if(newState.member.user.id === "1013135812545753119" || newState.member.user.id === "1149965771200544798") return;
 
-        const db = bot.db;
-        const oldChannel = oldState.channel;
-        const newChannel = newState.channel;
+  const db = bot.db;
+  const oldChannel = oldState.channel;
+  const newChannel = newState.channel;
 
-        db.query(`SELECT * FROM serveur WHERE guildID = ${oldState.guild.id}`, async (err, req, length) => {
-            if(req.length < 1) return;
+  db.query(
+    `SELECT * FROM serveur WHERE guildID = ${oldState.guild.id}`,
+    async (err, req, length) => {
+      if (req.length < 1) return;
 
-            let channel = oldState.guild.channels.cache.get(`${req[0].logID}`)
-            if(!channel) return;
+      let channel = oldState.guild.channels.cache.get(`${req[0].vocID}`);
+      if (!channel) return;
 
-        if (oldChannel === null && newChannel !== null) {
-            let Embed1 = new Discord.MessageEmbed()
-            .setColor("GREEN")
-            .setTitle("Salon vocal rejoint :")
-            .setDescription(`**${newState.member.user} - ${newState.member.user.username} *(${newState.member.user.id})* viens de rejoindre: \`\`${newChannel.name}\`\`**`)
-            .setTimestamp()
-            await channel.send({embeds: [Embed1]})
-
-        } else if (newChannel === null) {
-            let Embed2 = new Discord.MessageEmbed()
-            .setColor("RED")
-            .setTitle("Salon vocal quitter :")
-            .setDescription(`**${oldState.member.user} - ${oldState.member.user.username} *(${oldState.member.user.id})* viens de quitter: \`\`${oldChannel.name}\`\`**`)
-            .setTimestamp()
-            await channel.send({embeds: [Embed2]})
-        } else {
-            let Embed3 = new Discord.MessageEmbed()
-            .setColor("BLUE")
-            .setTitle("Salon vocal moove :")
-            .setDescription(`**${oldState.member.user} - ${oldState.member.user.username} *(${oldState.member.user.id})* à été moove de : \n \`\`${oldChannel.name}\`\` à \`\`${newChannel.name}\`\`**`)
-            .setTimestamp()
-            await channel.send({embeds: [Embed3]})
-        }})})
+      if (oldChannel === null && newChannel !== null) {
+        let Embed1 = new Discord.MessageEmbed()
+          .setColor("GREEN")
+        .setThumbnail(newState.member.displayAvatarURL({dynamic: true}))
+          .setTitle("Join Voice:")
+          .setDescription(`**${newState.member.user} *(${newState.member.user.id})* viens de rejoindre:** <#${newChannel.id}> `) // - ${newState.member.user.username} / \`\`${newChannel.name}\`\`
+          .setTimestamp();
+        await channel.send({ embeds: [Embed1] });
+      } else if (newChannel === null) {
+        let Embed2 = new Discord.MessageEmbed()
+          .setColor("RED")
+        .setThumbnail(oldState.member.displayAvatarURL({dynamic: true}))
+          .setTitle("Leave Voice:")
+          .setDescription(`**${oldState.member.user} *(${oldState.member.user.id})* viens de quitter:** <#${oldChannel.id}> `) // - ${oldState.member.user.username} \`\`${oldChannel.name}\`\`
+          .setTimestamp();
+        await channel.send({ embeds: [Embed2] });
+      } else if (oldChannel === newChannel) {
+        /*let Embed3 = new Discord.MessageEmbed()
+          .setColor("BLUE")
+          .setTitle("Salon vocal moove :")
+          .setDescription(`**${oldState.member.user} - ${oldState.member.user.username} *(${oldState.member.user.id})* à été moove de : \n \`\`${oldChannel.name}\`\` à \`\`${newChannel.name}\`\`**`)
+          .setTimestamp();
+        await channel.send({ embeds: [Embed3] }); */
+        return;
+      } else {
+        return;
+      }
+    }
+  );
+});

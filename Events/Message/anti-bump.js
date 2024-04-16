703954e708a02163479345d57ed2fdb9b6f2d6bc
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:27:10 +0200

    Update anti-bump.js

--- Events/Message/anti-bump.js
+++ Events/Message/anti-bump.js
@@ -3,10 +3,10 @@ const Event = require("../../Structure/Event")
 
 module.exports = new Event("messageCreate", async (bot, message, guild) => {
     if(message.author.bot) return;
+	if(message.guild === null) return;
 
-    const channels = "1065927640650891284"
-    const channel2 = "1120791058025029742"
+    const channels = "ID"
     const salon = bot.channels.cache.get(`${message.channel.id}`)
-    if(channels == message.channel.id) return message.delete() && salon.send(` <:elexyr22:1067501213085597806> ${message.author}, tu n'a pas droit **d'envoyé messages,** tu peux seulement __utilisé__ : \`\/bump\`\. <a:nop1:1068106487358038126> `).then(async mess => setTimeout(async () => {mess.delete()}, 5000)) 
-    if(channel2 == message.channel.id) return message.delete() && salon.send(` <:elexyr22:1067501213085597806> ${message.author}, tu n'a pas droit **d'envoyé messages,** tu peux seulement __utilisé__ : \`\/bump\`\. <a:nop1:1068106487358038126> `).then(async mess => setTimeout(async () => {mess.delete()}, 5000)) 
+    
+    if(channels == message.channel.id) return message.delete() && salon.send(`${message.author}, tu n'a pas droit **d'envoyer messages,** tu peux seulement __utilisé__ : \`\/bump\`\.`).then(async mess => setTimeout(async () => {mess.delete()}, 5000)) 
 })

96b111428f40ac11b4be8281be469522cbda1dc9
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 16:40:10 +0200

    Update 4k.js

--- Commandes/nfsw/4k.js
+++ Commandes/nfsw/4k.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js")
 const NSFW = require("discord-nsfw");
 const nsfw = new NSFW();
-const fs = require("fs");
 const Command = require("../../Structure/Command")
 
 module.exports = new Command({
@@ -9,25 +8,22 @@ module.exports = new Command({
     name: "4k",
     description: "|| Image en meuf en 4k ||",
     utilisation: "",
-    alias: ["4k",],
+    alias: ["4k"],
     permission: "",
     category: "6) NSFW",
-    cooldown: 15,
+    cooldown: 10,
 
-    async run(bot, message, args, db) {
+    async run(message) {
 
-        var errMessage = "Merci de créé un salon **NSFW,** pour utilisé cette commandes ! <:18:767648849035526165>";
+        var errMessage = "Merci de créé un salon **NSFW,** pour utilisé cette commandes ! :underage:";
         if (!message.channel.nsfw) {
             return message.reply(errMessage)}
         const image = await nsfw.fourk();
         const embed = new Discord.MessageEmbed()
-            .setTitle(`Image 4k =`)
+            .setTitle(`Image 4k:`)
             .setColor('RANDOM')
             .setTimestamp()
             .setFooter(`Demandé par : ${message.author.tag}`,message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 32 }))
             .setImage(image);
-            message.channel.send({embeds : [embed]})
-    
-    }
-        
-    });
+            message.reply({embeds : [embed]})
+}});

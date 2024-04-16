9b1aa92b6e5505dd009d503b08c1707677f5b061
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:09:14 +0200

    Update auto-restart.js

--- Events/Bot/auto-restart.js
+++ Events/Bot/auto-restart.js
@@ -10,7 +10,7 @@ module.exports = new Event("messageCreate", async (bot, message) => {
     if (err) {
       console.error(err);
       process.exit(0);
-       const channel = bot.channels.cache.get("1107619027452428288")  
+       const channel = bot.channels.cache.get("ID")  
        channel.send("La base de données à crach, j'ai bien restart")
       return;
     }

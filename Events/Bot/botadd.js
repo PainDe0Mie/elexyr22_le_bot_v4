553411aad6b9a3c68460a6c6d8344dd20693f09c
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:09:41 +0200

    Update botadd.js

--- Events/Bot/botadd.js
+++ Events/Bot/botadd.js
@@ -1,17 +1,22 @@
 const Discord = require("discord.js")
 const Event = require("../../Structure/Event");
 
-module.exports = new Event("guildCreate", async (bot, guild) => {
+module.exports = new Event("guildCreate", async (bot, guild, user) => {
 
-    console.log(`New serveur : ${guild.name} je suis à ${bot.guilds.cache.size}  serveur !`)
+console.log(`New serveur : ${guild.name} je suis à  ${bot.guilds.cache.size} serveur !`)
     
+let serveur = guild.channels.cache.find(channel => channel.type === "GUILD_TEXT" && channel.permissionsFor(bot.user.id).has("CREATE_INSTANT_INVITE"))
+if(!serveur) return;
+
+let invite = await serveur.createInvite({ maxAge: 0, maxUses: 0 })
     
-      const channel = bot.channels.cache.get("")  
+      const channel = bot.channels.cache.get("ID")  
       let addembed = new Discord.MessageEmbed()
         .setTitle(`Join de : ${guild.name} *(${guild.id})*`)
         .setThumbnail(guild.iconURL())
-        .addField(`👑 Propriétaire:`, `<@${guild.ownerId}> *(${guild.ownerId})*`)
+        .addField(`👑 Propriétaire:`, `<@${guild.ownerId}> - *(${guild.ownerId})*`)
         .addField(`Nombre de membres:`, `${guild.memberCount}`)
+      	.addField(`🛬 Invitation :`, `${invite}`)
         .setColor("11d646")
         .setTimestamp()
         .setFooter(`Merci grâce à toi nous sommes à ${bot.guilds.cache.size} serveurs`, bot.user.displayAvatarURL())

0711737eef9088b5858b9ab30c6398d16c2df040
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:00:51 +0200

    Update searchLinks.js

--- Fonctions/searchLinks.js
+++ Fonctions/searchLinks.js
@@ -1,13 +1,29 @@
+const regex = new RegExp(/https?:\/\/(www\.)?(tenor\.com|giphy\.com)[^\s]+/gi);
+
 module.exports = async (message, member, guild) => {
+  // Récupérer l'auteur du message
+  const author = message.author;
 
-   /* const msg = message.author
+  // Vérifier si l'auteur a le rôle d'administrateur
+  if (message.member.permissions.has("ADMINISTRATOR")) return //console.log("Admin Detecté")
 
-   if(msg.hasPermission("ADMINISTRATOR"))return message.reply("uwu") */
+  // Vérifier si le message contient un lien vers Tenor ou Giphy
+  if (message.content.match(regex)) return;
 
-    if(message.content.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g))) {
+  // Vérifier si le message contient un lien Discord.gg sans "https://"
+  const discordLinkRegex = new RegExp(/(www\.)?discord\.gg\/\w+/gi);
+  if (message.content.match(discordLinkRegex)) {
+    await message.delete();
+    await message.channel.send(` || ${author} ||, **les liens Discord.gg** ne sont pas __acceptés__ dans ce __salon !__ `)
+      .then(async mess => setTimeout(async () => { mess.delete() }, 10000));
+    return;
+  }
 
-        await message.delete()
-
-        await message.channel.send(`⚠️ Attention ${message.author}, les liens ne sont pas acceptés dans les salon !`)
-    }
-}
+  // Vérifier si le message contient un autre lien
+  const otherLinkRegex = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g);
+  if (message.content.match(otherLinkRegex)) {
+    await message.delete();
+    await message.channel.send(`| ${author} ||, **les liens de site** ne sont pas __acceptés__ dans ce __salon !__ `)
+      .then(async mess => setTimeout(async () => { mess.delete() }, 10000));
+  }
+};

71064297e6fa19a0670e170f1c56daa1b44cc6b6
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:03:52 +0200

    Update and rename ready.js to !ready.js

--- Events/Bot/!ready.js
+++ Events/Bot/!ready.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const Event = require("../../Structure/Event");
 const SlashCommand = require("../../Structure/SlashCommand");
+const chalk = require("chalk");
 
 module.exports = new Event("ready", async (bot) => {
   const db = bot.db;
@@ -18,11 +19,32 @@ module.exports = new Event("ready", async (bot) => {
       type: "STREAMING",
       url: "https://www.twitch.tv/elexyr22_",
     });
-  }, 5000);
+  }, 5000); 
 
-  console.log(
-    `${bot.user.username} (${bot.user.id}) : En ligne sur ${bot.guilds.cache.size} serveur(s) !`
-  );
+  // bot.user.setStatus('invisible'); // Mettre le bot en invisible
+
+  console.log(chalk.bgBlue(`${bot.user.username}: En ligne sur ${bot.guilds.cache.size} serveurs !`))
+
+  // Obtenez le serveur souhaité
+  let guild = bot.guilds.cache.get("ID");
+  if (!guild) return console.error("Serveur introuvable.");
+
+  // Obtenez le canal à partir du serveur
+  let start = guild.channels.cache.get("ID");
+  if (!start) return console.error("Canal introuvable dans le serveur.");
+
+  let now = new Date();
+  let Embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle("Bot Restart:")
+    .setDescription(`Le bot a été **start** le: "<t:${Math.floor(now.getTime() / 1000)}:F>"`)
+    .setTimestamp();
+  
+  try {
+    await start.send({embeds: [Embed]});
+  } catch (error) {
+    console.error("Erreur lors de l'envoi du message :", error);
+  }
 
   // Vérifiez les rappels toutes les secondes
   setInterval(async () => {
@@ -43,7 +65,7 @@ module.exports = new Event("ready", async (bot) => {
             const user = await bot.users.fetch(row.userID);
 
             if (user) {
-              await user.send(`<a:10:1077280299798495262> | Rappel: **${row.reason}**`);
+              await user.send(`Rappel: **${row.reason}**`);
             }
 
             // Supprimez l'entrée correspondante de la table "rmd" en fonction de la raison

63fa3cc5d5986efbab4c096b1cd3d16e682a0b0a
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:01:43 +0200

    Update searchSpam.js

--- Fonctions/searchSpam.js
+++ Fonctions/searchSpam.js
@@ -1,77 +1,62 @@
 const UserMap = new Map();
 
 module.exports = async (message, db, bot) => {
-    if(UserMap.get(message.author.id)) {
+  if (UserMap.get(message.author.id)) {
+    const UserData = UserMap.get(message.author.id);
+    const { lastMessage, timer } = UserData;
+    const difference = message.createdTimestamp - lastMessage.createdTimestamp;
+    let msgCount = UserData.msgCount;
 
-        const UserData = UserMap.get(message.author.id)
-        const { lastMessage, timer } = UserData
-        let difference = message.createdTimestamp - lastMessage.createdTimestamp;
-        let msgCount = UserData.msgCount;
+    if (difference > 3000) {
+      clearTimeout(timer);
+      UserData.msgCount = 1;
+      UserData.lastMessage = message;
 
-        if(difference > 3000) {
+      UserData.timer = setTimeout(() => {
+        UserMap.delete(message.author.id);
+      }, 10000);
 
-            clearTimeout(timer);
-            UserData.msgCount = 1;
-            UserData.lastMessage = message;
-
-            UserData.timer = setTimeout(() => {
-
-                UserMap.delete(message.author.id)
-
-            }, 10000)
-
-            UserMap.set(message.author.id, UserData)
-        
-        } else {
-
-            msgCount++;
-
-            if(msgCount >= 5) {
-
-                await message.channel.send(`⚠️ Attention ${message.author}, vous envoyez trop de messages en peu de temps !`)
-
-                const messages = [...(await message.channel.messages.fetch({
-                    limit: 5,
-                    before: message.id,
-                })).filter(msg => msg.author.id === message.author.id).values()]
-
-                await message.channel.bulkDelete(messages);
-            
-            } else {
-
-                UserData.msgCount = msgCount;
-                UserMap.set(message.author.id, UserData)
-            }
-            if(msgCount >= 7) {
-
-                await message.guild.members.cache.get(message.author.id).ban({reason: `⚠️ Detection de SPAM`})
-
-                const messages = [...(await message.channel.messages.fetch({
-                    limit: 8,
-                    before: message.id,
-                })).filter(msg => msg.author.id === message.author.id).values()]
-
-                await message.channel.bulkDelete(messages);
-            
-            } else {
-
-                UserData.msgCount = msgCount;
-                UserMap.set(message.author.id, UserData)
-            }
-        }
-
+      UserMap.set(message.author.id, UserData);
     } else {
+      msgCount++;
 
-        let fn = setTimeout(async () => {
+      if (msgCount >= 5) {
+        const member = message.guild.members.cache.get(message.author.id);
+        await message.channel.send(
+          `⚠️ Attention ${message.author}, vous envoyez trop de messages en peu de temps !`
+        );
 
-            UserMap.delete(message.author.id)
-        }, 10000)
+        await member.ban({ reason: `⚠️ Detection de SPAM` });
+        await message.channel.send(
+          `L'utilisateur ${member} a été banni pour spam.`
+        );
 
-        UserMap.set(message.author.id, {
+        await message.guild.members.unban(member, `Réhabilitation de l'utilisateur après bannissement pour spam.`);
 
-            msgCount: 1,
-            lastMessage: message,
-            timer: fn
-        })
+        const messages = [
+          ...(await message.channel.messages.fetch({
+            limit: 5,
+            before: message.id,
+          }))
+            .filter((msg) => msg.author.id === message.author.id)
+            .values(),
+        ];
+
+        await message.channel.bulkDelete(messages);
+      } else {
+        UserData.msgCount = msgCount;
+        UserMap.set(message.author.id, UserData);
+      }
     }
-}
+  } else {
+    let fn = setTimeout(async () => {
+      UserMap.delete(message.author.id);
+    }, 10000);
+
+    UserMap.set(message.author.id, {
+      msgCount: 1,
+      lastMessage: message,
+      timer: fn,
+    });
+  }
+};

ccb0e3fdc55a4f6c60f2cfa165e74a6df3b02224
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 16:43:47 +0200

    Update chatte.js

--- Commandes/nfsw/chatte.js
+++ Commandes/nfsw/chatte.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js")
 const NSFW = require("discord-nsfw");
 const nsfw = new NSFW();
-const fs = require("fs");
 const Command = require("../../Structure/Command")
 
 module.exports = new Command({
@@ -12,23 +11,20 @@ module.exports = new Command({
     alias: ["chatte",],
     permission: "",
     category: "6) NSFW",
-    cooldown: 15,
+    cooldown: 10,
 
     async run(bot, message, args, db) {
 
-        var errMessage = "Merci de créé un salon **NSFW,** pour utilisé cette commandes ! <:18:767648849035526165>";
+        var errMessage = "Merci de créé un salon **NSFW,** pour utilisé cette commandes ! :underage:";
         if (!message.channel.nsfw) {
             return message.reply(errMessage)}
 
             const image = await nsfw.pussy();
             const embed = new Discord.MessageEmbed()
-                .setTitle(`Image de Chatte =`)
+                .setTitle(`Image de Chatte:`)
                 .setColor('RANDOM')
                 .setTimestamp()
                 .setFooter(`Demandé par : ${message.author.tag}`,message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 32 }))
                 .setImage(image);
-            message.channel.send({embeds : [embed]})
-    
-    }
-        
-    });
+                message.reply({embeds : [embed]})
+}});

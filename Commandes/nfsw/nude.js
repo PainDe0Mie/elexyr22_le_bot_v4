4073a98ddc488904416f9ef2e0d9346be5eb47cf
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 16:49:31 +0200

    Update nude.js

--- Commandes/nfsw/nude.js
+++ Commandes/nfsw/nude.js
@@ -5,27 +5,26 @@ const Command = require("../../Structure/Command")
 
 module.exports = new Command({
 
-    name: "nude",
-    description: "|| Image d'une meuf nue ||",
+    name: "seins",
+    description: "|| Image d'un seins ||",
     utilisation: "",
-    alias: ["nude",],
+    alias: ["seins",],
     permission: "",
     category: "6) NSFW",
     cooldown: 10,
 
     async run(message) {
 
-        var errMessage = "Merci de créé un salon **NSFW,** pour utilisé cette commandes ! :underage:";
+        var errMessage = "<:elexyr22:1067501213085597806> Merci de créé un salon **NSFW,** pour utilisé cette commandes ! :underage:";
         if (!message.channel.nsfw) {
             return message.reply(errMessage)}
 
-            const image = await nsfw.ass();
+            const image = await nsfw.boobs();
             const embed = new Discord.MessageEmbed()
-                .setTitle(`Voici une Nude Random:`)
+                .setTitle(`Image de seins = `)
                 .setColor('RANDOM')
                 .setTimestamp()
                 .setFooter(`Demandé par : ${message.author.tag}`,message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 32 }))
                 .setImage(image);
                 message.reply({embeds : [embed]})
-    
     }});

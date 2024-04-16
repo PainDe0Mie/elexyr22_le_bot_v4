8a19e652907aaf55723d4327c73007cb843d081f
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 12:02:35 +0200

    Create auto-role-bot.js

---
+++ Commandes/utile/auto-role-bot.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js")
+const Command = require("../../Structure/Command")
+
+module.exports = new Command({
+
+    name: "auto-role-bot",
+    description: "Permet d'avoir un auto-rôle sûr les bots",
+    utilisation: "",
+    alias: ["auto-role-bot", "bot"],
+    permission: Discord.Permissions.FLAGS.MANAGE_GUILD,
+    category: "3) Utile",
+    cooldown: 5,
+    
+    async run(bot, message, args, db) {
+        try {
+            let roleID = args[0];
+            if (!roleID || isNaN(roleID)) {
+                return message.reply("<:elexyr22:1067501213085597806> Veuillez m'indiquer l'ID d'un rôle valide.");
+            } else {
+                const role = message.guild.roles.cache.get(roleID);
+                if (!role) {
+                    return message.reply("<:elexyr22:1067501213085597806> Le rôle avec cet ID n'existe pas dans ce serveur.");
+                }
+
+                // Mise à jour de la base de données avec l'ID du rôle
+                sql = `UPDATE serveur SET roleBOT = '${roleID}' WHERE guildID = ${message.guild.id}`;
+                db.query(sql, function (err) {
+                    if (err) throw err;
+                });
+
+                message.reply(`<:elexyr22:1067501213085597806> **L'auto-Rôle-Bot** est bien __activé__ sûr ${role} !`);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+});

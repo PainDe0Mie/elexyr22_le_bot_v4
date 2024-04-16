82b08683abe810516e056369e9239f68c6de6fb0
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:16:22 +0200

    Update page-top.js

--- Events/Interaction/page-top.js
+++ Events/Interaction/page-top.js
@@ -6,68 +6,87 @@ module.exports = new Event("interactionCreate", async (bot, interaction) => {
     
     const db = bot.db
     if(interaction.isButton()) {
-    if(interaction.customId === "level") {
+        if(interaction.customId === "level") {
 
-          db.query(`SELECT userID, level FROM user ORDER BY level DESC LIMIT 10`,
+            db.query(`SELECT userID, level FROM user ORDER BY level DESC LIMIT 10`,
             async (err, results) => {
-              if (err) throw err;
-      
-              const embed1 = new Discord.MessageEmbed()
+                if (err) throw err;
+
+                const embed1 = new Discord.MessageEmbed()
                 .setColor(bot.color)
                 .setTitle("Top 10 | LEVEL :");
                 
-              for (let i = 0; i < results.length; i++) {
-                const user = await bot.users.fetch(results[i].userID);
-                embed1.addField(
-                  `Rang ${i + 1}: ${user.username}`,
-                  `Niveau: \`\`${results[i].level}\`\``)}
-                  interaction.update({ embeds: [embed1]});
-            })}
-
-if(interaction.customId === "vocal") {
-  db.query(`SELECT userID, voiceTime FROM user ORDER BY voiceTime DESC LIMIT 10`,
-  async (err, results) => {
-    if (err) throw err;
-
-    const embed1 = new Discord.MessageEmbed()
-      .setColor(bot.color)
-      .setTitle("Top 10 | VOCAL :");
-    for (let i = 0; i < results.length; i++) {
-      const user = await bot.users.fetch(results[i].userID);
-      embed1.addField(
-        `Rang ${i + 1}: ${user.username}`,
-        `Niveau: \`\`${results[i].voiceTime}\`\``)}
-        interaction.update({ embeds: [embed1]});
- })}
-
-          if(interaction.customId === "bump") {
-            db.query(`SELECT userID, bump FROM user ORDER BY bump DESC LIMIT 10`,
-              async (err, results) => {
-                if (err) throw err;
-        
-                const embed1 = new Discord.MessageEmbed()
-                  .setColor(bot.color)
-                  .setTitle("Top 10 | BUMP :");
                 for (let i = 0; i < results.length; i++) {
-                  const user = await bot.users.fetch(results[i].userID);
-                  embed1.addField(
-                    `Rang ${i + 1}: ${user.username}`,
-                    `Niveau: \`\`${results[i].bump}\`\``)}
-                    interaction.update({ embeds: [embed1]});
-              })}
-
-              if(interaction.customId === "event") {
-                db.query(`SELECT userID, event FROM user ORDER BY event DESC LIMIT 10`,
-                  async (err, results) => {
-                    if (err) throw err;
-            
-                    const embed1 = new Discord.MessageEmbed()
-                      .setColor(bot.color)
-                      .setTitle("Top 10 | EVENT :");
-                    for (let i = 0; i < results.length; i++) {
-                      const user = await bot.users.fetch(results[i].userID);
-                      embed1.addField(
+                    const user = await bot.users.fetch(results[i].userID);
+                    embed1.addField(
                         `Rang ${i + 1}: ${user.username}`,
-                        `Niveau: \`\`${results[i].event}\`\``)}
-                      interaction.update({ embeds: [embed1]});
-})}}})
+                        `Niveau: \`\`${results[i].level}\`\``
+                    );
+                }
+                interaction.update({ embeds: [embed1] });
+            })
+        }
+
+        if(interaction.customId === "vocal") {
+            db.query(`SELECT userID, voiceTime FROM user ORDER BY voiceTime DESC LIMIT 10`,
+            async (err, results) => {
+                if (err) throw err;
+
+                const embed1 = new Discord.MessageEmbed()
+                .setColor(bot.color)
+                .setTitle("Top 10 | VOCAL :");
+
+                for (let i = 0; i < results.length; i++) {
+                    const user = await bot.users.fetch(results[i].userID);
+                    embed1.addField(
+                        `Rang ${i + 1}: ${user.username}`,
+                        `Niveau: \`\`${results[i].voiceTime}\`\``
+                    );
+                }
+                interaction.update({ embeds: [embed1] });
+            })
+        }
+
+        if(interaction.customId === "bump") {
+            db.query(`SELECT userID, CAST(bump AS DECIMAL(10, 2)) AS bump FROM user ORDER BY bump DESC LIMIT 10`,
+            async (err, results) => {
+                if (err) throw err;
+
+                const embed1 = new Discord.MessageEmbed()
+                .setColor(bot.color)
+                .setTitle("Top 10 | BUMP :");
+
+                for (let i = 0; i < results.length; i++) {
+                    const user = await bot.users.fetch(results[i].userID);
+                    const formattedBump = results[i].bump.toFixed(2).replace(/\.00$/, '');
+
+                    embed1.addField(
+                        `Rang ${i + 1}: ${user.username}`,
+                        `Bumps: \`\`${formattedBump}\`\``
+                    );
+                }
+                interaction.update({ embeds: [embed1] });
+            })
+        }
+
+        if(interaction.customId === "event") {
+            db.query(`SELECT userID, event FROM user ORDER BY event DESC LIMIT 10`,
+            async (err, results) => {
+                if (err) throw err;
+
+                const embed1 = new Discord.MessageEmbed()
+                .setColor(bot.color)
+                .setTitle("Top 10 | EVENT :");
+
+                for (let i = 0; i < results.length; i++) {
+                    const user = await bot.users.fetch(results[i].userID);
+                    embed1.addField(
+                        `Rang ${i + 1}: ${user.username}`,
+                        `Niveau: \`\`${results[i].event}\`\``
+                    );
+                }
+                interaction.update({ embeds: [embed1] });
+            })
+        }
+    }
+});

bddbe3539779a8b5c315b4ed8022ff209108be6a
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:01:25 +0200

    Update searchMentions.js

--- Fonctions/searchMentions.js
+++ Fonctions/searchMentions.js
@@ -1,17 +1,22 @@
+const userWarns = new Map();
+
 module.exports = async (message) => {
-
-    let content = message.content.split(" ")
-    let count = 0;
+  const mentions = message.mentions.users.size + message.mentions.roles.size;
+  if (mentions > 2) {
+    await message.delete();
+    await message.channel.send(`${message.author}, vous mentionnez trop de fois dans un seul message !`);
     
+    const member = await message.guild.members.fetch(message.author);
+    let warns = userWarns.get(member.id) || 0;
 
-    for(let i = 0; i < content.length; i++) {
-
-        if(content[i].match(new RegExp(/<@!*&*[0-9]+>/g))) count++;
+    if (warns >= 2) {
+      await member.kick('Trop de mentions dans un message');
+      await message.channel.send(`L'utilisateur ${member} a été kick pour trop de mentions dans un message `);
+      userWarns.delete(member.id);
+    } else {
+      warns++;
+      userWarns.set(member.id, warns);
+      await message.channel.send(`Attention ${member}, vous avez été averti pour avoir mentionné trop de fois dans un seul message (${warns}/3)`);
     }
-
-    if(count > 5) {
-
-        await message.delete()
-        await message.channel.send(`⚠️ Attention ${message.author}, vous mentionnez trop de fois dans un seul message !`)
-    }
-}
+  }
+};

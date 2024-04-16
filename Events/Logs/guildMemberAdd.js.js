7eca817a8740630cdf23aca843d489942dde3b34
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:22:09 +0200

    Update guildMemberAdd.js.js

--- Events/Logs/guildMemberAdd.js.js
+++ Events/Logs/guildMemberAdd.js.js
@@ -4,24 +4,10 @@ const Event = require("../../Structure/Event")
 const ms = require("ms");
 
 module.exports = new Event("guildMemberAdd", async (bot, member, user) => {
+    if(member.guild.id === "ID") return; //id du serv
     
     const db = bot.db;
-    console.log(chalk.blue(`[JOIN SERVEUR :] ${member.user.username} viens de rejoindre ${member.guild.name} (${member.guild.id}) !`))
-
-    db.query(`SELECT * FROM joins WHERE userID = ${member.user.id}`, async (err, req) => {
-        if(member.guild.id !== "1040701512298541106") return;
-        if(req.length < 1) {
-
-            let sql = `INSERT INTO joins (userID, number)VALUES (${member.user.id}, '1')`
-            db.query(sql, function(err) {
-                if(err) throw err;
-
-        })} else {
-		let xp = 1
-            sql = `UPDATE joins SET number = '${parseInt(req[0].number) + xp}' WHERE userID = ${member.user.id}`
-            db.query(sql, function (err){
-              if(err) throw err;
-            })}
+    console.log(chalk.blue(`[JOIN SERVEUR :] ${member.user.username} viens de rejoindre ${member.guild.name} (${member.guild.id}) !`))   
     
       let triggerTime = Date.now()
       let timestampInMilliseconds = triggerTime
@@ -33,7 +19,7 @@ module.exports = new Event("guildMemberAdd", async (bot, member, user) => {
     .setColor("GREEN")
     .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
     .setTitle("New Join :")
-    .setDescription(`**User:** ${member} | **_(${member.user.username})_** \n\n **ID:** \`\`${member.id}\`\`\n\n **A Rejoint:** <t:${timestampInSeconds}:R> \n\n **Compte Créé le:** <t:${Math.floor(member.user.createdAt / 1000)}:F>\n\n **A rejoint:** \`\`${req[0].number}\`\` __/ fois__`) //\n\n Join: \`\`${member.guild.name}\`\`**
+    .setDescription(`**User:** ${member} | **_(${member.user.username})_** \n\n **ID:** \`\`${member.id}\`\`\n\n **A Rejoint:** <t:${timestampInSeconds}:R> \n\n **Compte Créé le:** <t:${Math.floor(member.user.createdAt / 1000)}:F>`) //\n\n Join: \`\`${member.guild.name}\`\`**
      .setImage(await (await bot.users.fetch(member.user, {force: true})).bannerURL({dynamic: true, size: 4096}))
      .setTimestamp()
 
@@ -44,4 +30,4 @@ module.exports = new Event("guildMemberAdd", async (bot, member, user) => {
     if(!channel) return;
     await channel.send({embeds: [Embed]})
 
-})})})})
+})})})

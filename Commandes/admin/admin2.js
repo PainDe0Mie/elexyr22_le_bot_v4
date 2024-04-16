12b3d6dd2c1ed0a051d8c4850689aa6a1b8f4ae0
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 10:36:16 +0200

    Update admin2.js

--- Commandes/admin/admin2.js
+++ Commandes/admin/admin2.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js")
-const Command = require("../../Structure/Command")
+const Discord = require("discord.js");
+const Command = require("../../Structure/Command");
 const chalk = require("chalk");
 
 module.exports = new Command({
@@ -14,20 +14,28 @@ module.exports = new Command({
 
     async run(bot, message, guild) {
         
-    const db = bot.db;
-      
-    const user = message.author
+        const db = bot.db;
+        const user = message.author;
 
-    db.query(`SELECT * FROM admin WHERE userID = ${user.id}`, async (err, req) => {
+        db.query(`SELECT * FROM admin WHERE userID = ${user.id}`, async (err, req) => {
 
-	if(req.length < 1) return message.reply(" Uniquement les **Admins** peut utilisé cette commande !") 
-
-   if(req[0].statut === "OFF") return message.reply(" Uniquement les **Admins** peut utilisé cette commande !") 
-  if(req[0].statut === "ACTIF") {
-        
-
-    message.guild.roles.cache.find(role => role.name === '👑・PERMISSION ANTI-RAID').delete()
-    message.reply(" **Mode Admin** désactivé !")
- 	console.log(chalk.bgBlue(`[Staff Retiré] pour ${message.author.username}  | sûr ${message.guild.name}`))
-        
-    }})}})
+            if(req.length < 1) return message.reply(" Uniquement les **Admins** peuvent utiliser cette commande ! ");
+            
+            if(req[0].statut === "OFF") return message.reply(" Uniquement les **Admins** peuvent utiliser cette commande ! ");
+            
+            if(req[0].statut === "ACTIF") {
+                try {
+                    const role = message.guild.roles.cache.find(role => role.name === '👑・PERMISSION ANTI-RAID');
+                    if (!role) throw new Error("Le rôle n'a pas été trouvé !");
+                    
+                    await role.delete();
+                    message.reply(" **Mode Admin** désactivé !");
+                    console.log(chalk.bgBlue(`[Staff Retiré] pour ${message.author.username}  | sur ${message.guild.name}`));
+                } catch (error) {
+                    console.error(error);
+                    message.reply(" *Une erreur est survenue lors de la suppression du rôle...*");
+                }
+            }
+        });
+    }
+});

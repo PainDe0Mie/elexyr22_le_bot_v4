02b99aaa591dd607c294f959f256c95b7dc397dc
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-16 16:27:19 +0200

    Update clear.js

--- Commandes/mod/clear.js
+++ Commandes/mod/clear.js
@@ -11,38 +11,32 @@ module.exports = new Command({
   category: "1) Modération",
   cooldown: 5,
 
-  async run(bot, message, args, db, guild) {
+  async run(bot, message, args) {
     
-    message.delete()
     let number = args[0] || args._hoistedOptions[0].value;
     if (isNaN(number)) {
-      return message.reply(
-        "Veuillez indiquer un nombre entre `0` et `100` !"
-      ).then(async (mess) => {
-        setTimeout(async () => {
-          mess.delete();
-        }, 5000);
+      return message.reply("*Veuillez indiquer un nombre entre `0` et `100` !* ").then(async (mess) => {setTimeout(async () => {mess.delete()}, 5000);
       });
     }
     if (parseInt(number) <= 0 || parseInt(number) > 100) {
-      return message.reply("Veuillez indiquer un nombre entre `0` et `100` !").then(async (mess) => {setTimeout(async () => {mess.delete();}, 5000)})}
+      return message.reply("*Veuillez indiquer un nombre entre `0` et `100`...* ").then(async (mess) => {setTimeout(async () => {mess.delete();}, 5000)})}
 
     const messagesToDelete = parseInt(number);
     try {
       const fetchedMessages = await message.channel.messages.fetch({ limit: messagesToDelete + 1 });
       const deletedMessages = await message.channel.bulkDelete(fetchedMessages, true);
 
-      console.log(chalk.yellow(`[CMD] "${message.author.tag}" a utilisé la commande e!clear ${number} sur '${message.guild.name}'`))
+      console.log(chalk.yellow(`[CMD] "${message.author.username}" a utilisé la commande e!clear ${number} sur '${message.guild.name}'`))
 
-      if (message.guild.id === "1040701512298541106") {
-        const salon = bot.channels.cache.get("1084195196583018536");
-        salon.send(` ${message.author.tag} a utilisé la commande **e!clear ${number}.** `);
+      if (message.guild.id === "ID") { //id du serv
+        const salon = bot.channels.cache.get("ID");
+        salon.send(` ${message.author.username} a utilisé la commande **e!clear ${number}.**`);
       }
 
       await message.channel.send(`J'ai bien supprimé \`${deletedMessages.size - 1}\` messages !`).then(async (mess) => {setTimeout(async () => {mess.delete();}, 5000)})
     } catch (error) {
       console.error(error);
-      await message.channel.send("Une erreur s'est produite lors de la suppression des messages.").then(async (mess) => {setTimeout(async () => {mess.delete();}, 5000)})
+      await message.channel.send("*Yo, mec, y'a eu un couac en voulant virer les messages, tu vois. Ça a foiré grave.*").then(async (mess) => {setTimeout(async () => {mess.delete();}, 5000)})
     }
   },
 });

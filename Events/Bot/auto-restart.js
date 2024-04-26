c2a78da1d8782338b1dd5b546f680b10ac4005fc
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-26 16:14:58 +0200

    Update auto-restart.js

--- Events/Bot/auto-restart.js
+++ Events/Bot/auto-restart.js
@@ -1,21 +1,14 @@
 const Discord = require("discord.js");
 const Event = require("../../Structure/Event");
 
+module.exports = new Event("ready", async (bot) => {
+  const db = bot.db;
 
-module.exports = new Event("messageCreate", async (bot, message) => {
-    
-    const db = bot.db
-
-  db.query(`SELECT * FROM serveur WHERE guildID = ${message.guildId}`, async (err, req) => {
-    if (err) {
-      console.error(err);
-      process.exit(0);
-       const channel = bot.channels.cache.get("ID")  
-       channel.send("La base de données à crach, j'ai bien restart")
-      return;
-    }
-    if (req.length < 1) {
-    }
-  });
-
-});
+    db.query(`SELECT * FROM rmd`, async (err, req) => {
+      if (err) {
+        console.error(err);
+        process.exit(0);
+        return;
+      }
+    });
+  }, 10000);

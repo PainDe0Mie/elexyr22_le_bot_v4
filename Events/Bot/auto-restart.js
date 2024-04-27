184919c0e3da217802a335ebcfc64fd93a1ff40d
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-04-27 21:02:08 +0200

    Update auto-restart.js

--- Events/Bot/auto-restart.js
+++ Events/Bot/auto-restart.js
@@ -2,13 +2,27 @@ const Discord = require("discord.js");
 const Event = require("../../Structure/Event");
 
 module.exports = new Event("ready", async (bot) => {
-  const db = bot.db;
 
-    db.query(`SELECT * FROM rmd`, async (err, req) => {
-      if (err) {
-        console.error(err);
-        process.exit(0);
-        return;
-      }
-    });
-  }, 10000);
+  const db = bot.db;
+  const executeQuery = async () => {
+    try {
+      const result = await new Promise((resolve, reject) => {
+        db.query("SELECT * FROM rmd", (err, req) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(req);
+          }
+        });
+      });
+
+      console.log("Connexion à la base de données réussie");
+
+    } catch (error) {
+      console.error("Erreur lors de la connexion à la base de données :", error);
+      process.exit(1); 
+    }}
+    
+  await executeQuery();
+  setInterval(executeQuery, 10000);
+});

8ca1e464dcf794ca2a0e056a603f770bdfe825a6
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2024-08-09 12:39:21 +0200

    Update botremove.js
    Si le serveur est supprimé, n'envoye aucun message !

--- Events/Bot/botremove.js
+++ Events/Bot/botremove.js
@@ -5,12 +5,18 @@ module.exports = new Event("guildDelete", async (bot, guild) => {
     console.log(`Serveur supprimé : ${guild.name}. Je suis maintenant sur ${bot.guilds.cache.size} serveurs.`);
 
     // Code pour envoyer des informations sur le serveur supprimé à un canal spécifique
-    const channel = bot.channels.cache.get("id");
-    if (!channel) return;
+    const channel = bot.channels.cache.get("ID CHANNEL");
+    if (!channel) return; // Vérifie si le canal de log existe
+
+    // Vérification des propriétés du serveur pour s'assurer qu'elles ne sont pas nulles ou indéfinies
+    if (!guild.name || !guild.id || !guild.ownerId || !guild.memberCount) {
+        console.log("Certaines informations du serveur sont manquantes ou invalides. Aucune information ne sera envoyée.");
+        return;
+    }
 
     let removeEmbed = new Discord.MessageEmbed()
         .setTitle(`Départ de : ${guild.name} *(${guild.id})*`)
-        .setThumbnail(guild.iconURL())
+        .setThumbnail(guild.iconURL() || "default_thumbnail_url") // Vous pouvez fournir une URL de vignette par défaut ici
         .addField(`👑 Propriétaire:`, `<@${guild.ownerId}> - *(${guild.ownerId})*`)
         .addField(`Nombre de membres:`, `${guild.memberCount}`)
         .setColor("ff0000")
